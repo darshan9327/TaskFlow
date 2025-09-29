4f0030827f21b3bd4a2c9ec6bad1a728789e0e4d
Darshan <darshanpithadiya134@gmail.com>
2025-09-29 17:33:29 +0530

    changes in main.dart for blank screen error 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:task_flow/firebase_options.dart';
 
 import 'data/wrapper/wrapper.dart';
 import 'presentation/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
